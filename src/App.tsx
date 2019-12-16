5b49d55e2f7c6938aaa3cf214b0f2dcdcc9317ed
Kevin Hubert <iamkevinhubert@gmail.come>
2019-12-16 13:39:40 -0600

    pushing to heroku

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,71 @@
-import React, { useState } from "react";
-import { data } from "./raid-data";
-import { tsConstructorType } from "@babel/types";
+import React, { useState, useEffect } from 'react';
+import { data } from './raid-data';
+import { tsConstructorType } from '@babel/types';
 
-interface State {
-  currentRaid: object;
-}
-class App extends React.Component<{}, { currentRaid: object }> {
-  constructor(props: any) {
-    super(props);
+function App() {
+  const [raid, setRaid] = useState(data[0]);
+
+  function filterRaid(id: number) {
+    const currentRaid = data.filter(raid => raid.id === id);
+    console.log(currentRaid[0]);
+    setRaid(currentRaid[0]);
   }
 
-  render() {
-    return (
-      <div className="App">
-        <div className="container">
-          <div className="row header-primary">
-            <div className="col-12">
-              <h1 className="text-center">Classic Raid Loot</h1>
-            </div>
+  useEffect(() => {
+    console.log(raid.loot);
+  });
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row header-primary">
+          <div className="col-12">
+            <h1 className="text-center">Classic Raid Loot</h1>
           </div>
-          <div className="row">
-            <div className="col-md-3">
-              <div className="list-group">
-                {data.map(raid => (
-                  <a
-                    key={raid.id}
-                    href="#"
-                    className="list-group-item list-group-item-action"
-                  >
-                    {raid.name}
-                  </a>
-                ))}
-              </div>
-            </div>
-            <div className="col-md-9">
-              {data[0].loot.map(reward => (
-                <div className="reward">
-                  <div className="reward__character">
-                    <div className="reward__character--name">
-                      {reward.playerName.charAt(0).toUpperCase() +
-                        reward.playerName.substring(1)}
-                    </div>
-                    <div className="reward__character--class">
-                      {reward.playerClass.charAt(0).toUpperCase() +
-                        reward.playerClass.substring(1)}
-                    </div>
-                  </div>
-                  <div className="reward__loot">
-                    <a href="#" data-wowhead={`item=${reward.itemID}`}></a>
-                  </div>
-                  <div className="reward__boss">
-                    <a
-                      href="#"
-                      data-wowhead={`npc=${reward.npcID}&domain=classic`}
-                    ></a>
-                  </div>
-                </div>
+        </div>
+        <div className="row">
+          <div className="col-md-3">
+            <div className="list-group">
+              {data.map(raid => (
+                <a
+                  onClick={() => filterRaid(raid.id)}
+                  key={raid.id}
+                  href="#"
+                  className="list-group-item list-group-item-action"
+                >
+                  {raid.name}
+                </a>
               ))}
             </div>
           </div>
+          <div className="col-md-9">
+            {raid.loot.map(reward => (
+              <div className="reward">
+                <div className="reward__character">
+                  <div className="reward__character--name">
+                    {reward.playerName.charAt(0).toUpperCase() +
+                      reward.playerName.substring(1)}
+                  </div>
+                  <div className="reward__character--class">
+                    {reward.playerClass.charAt(0).toUpperCase() +
+                      reward.playerClass.substring(1)}
+                  </div>
+                </div>
+                <div className="reward__loot">
+                  <a href="#" data-wowhead={`item=${reward.itemID}`}></a>
+                </div>
+                <div className="reward__boss">
+                  <a
+                    href="#"
+                    data-wowhead={`npc=${reward.npcID}&domain=classic`}
+                  ></a>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 export default App;
