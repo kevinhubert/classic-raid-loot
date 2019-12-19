8220fc39d1acc03471a76ab649605c2dbe35a83b
Kevin Hubert <iamkevinhubert@gmail.come>
2019-12-19 11:19:59 -0600

    adding title and timeout

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,7 @@ function App() {
             </div>
           </div>
           <div className="col-md-9">
+            <h3 className="text-center">{raid.name}</h3>
             {raid.loot.map((reward, index) => (
               <div className="reward" key={index}>
                 <div className="reward__character">
