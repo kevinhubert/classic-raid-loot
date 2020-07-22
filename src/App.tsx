ebc297dd146d7ef84fad0d67638fcea538808a15
Kevin Hubert <kevin.hubert@buildasign.com>
2020-07-22 13:18:28 -0500

    refactoring app to new raid

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import { data } from './raid-data';
+import React, { useState, useEffect } from "react";
+import { data } from "./data/raid-data";
+
+const { dkptable } = data;
 
 function App() {
-  const [raid, setRaid] = useState(data[0]);
-
-  function filterRaid(id: number) {
-    const currentRaid = data.filter(raid => raid.id === id);
-    setRaid(currentRaid[0]);
-  }
-
-  useEffect(() => {});
-
   return (
     <div className="App">
       <div className="container">
         <div className="row header-primary">
-          <div className="col-12">
-            <h1 className="text-center">Classic Raid Loot</h1>
+          <div className="col-md-12">
+            <h1 className="top-header">Classic Raid Loot - The Departed R2</h1>
           </div>
         </div>
         <div className="row">
-          <div className="col-md-3">
-            <div className="list-group">
-              {data.map(raid => (
-                <a
-                  onClick={() => filterRaid(raid.id)}
-                  key={raid.id}
-                  href="#"
-                  className="list-group-item list-group-item-action"
-                >
-                  {raid.name}
-                </a>
-              ))}
-            </div>
-          </div>
-          <div className="col-md-9">
-            <h3 className="text-center">{raid.name}</h3>
-            {raid.loot.map((reward, index) => (
-              <div className="reward" key={index}>
-                <div className="reward__character">
-                  <div className="reward__character--name">
-                    {reward.playerName.charAt(0).toUpperCase() +
-                      reward.playerName.substring(1)}
+          <div className="col-md-12">
+            {dkptable.map((player) => (
+              <div className="list-item">
+                <div className="list-item__character">
+                  <div className="list-item__character--name">
+                    {player.player}
                   </div>
-                  <div className="reward__character--class">
-                    {reward.playerClass.charAt(0).toUpperCase() +
-                      reward.playerClass.substring(1)}
+                  <div className="list-item__character--class">
+                    {player.class}
+                    <div>
+                      <img
+                        src={`https://wow.zamimg.com/images/wow/icons/large/classicon_${player.class.toLowerCase()}.jpg`}
+                        alt=""
+                      />
+                    </div>
                   </div>
                 </div>
-                <div className="reward__loot">
-                  <a
-                    className="reward__loot--item"
-                    href="#"
-                    data-wowhead={`item=${reward.itemID}`}
-                  ></a>
-                </div>
-                <div className="reward__boss">
-                  <a
-                    href="#"
-                    data-wowhead={`npc=${reward.npcID}&domain=classic`}
-                  ></a>
+                <div className="list-item__dkp">
+                  <div className="list-item__dkp--current">
+                    Current DKP: {player.dkp}
+                  </div>
+                  <div className="list-item__dkp--gained">
+                    Lifetime Gained: {player.lifetimegained}
+                  </div>
+                  <div className="list-item__dkp--spent">
+                    Lifetime Spent: {Number(player.lifetimespent) * -1}
+                  </div>
                 </div>
               </div>
             ))}
