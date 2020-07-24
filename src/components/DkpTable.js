9e0d607c1792fb850a49f27a592bb9c35ad29144
Kevin Hubert <kevin.hubert@buildasign.com>
2020-07-24 16:16:28 -0500

    adding changes

--- src/components/DkpTable.js
+++ src/components/DkpTable.js
@@ -2,33 +2,37 @@ import React from "react";
 
 const DkpTable = (props) => (
   <div className="col-md-12">
-    {props.data.map((player, index) => (
-      <div className="list-item" key={index}>
-        <div className="list-item__character">
-          <div className="list-item__character--name">{player.player}</div>
-          <div className="list-item__character--class">
-            {player.class}
-            <div>
-              <img
-                src={`https://wow.zamimg.com/images/wow/icons/large/classicon_${player.class.toLowerCase()}.jpg`}
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
-        <div className="list-item__dkp">
-          <div className="list-item__dkp--current">
-            Current DKP: {player.dkp}
-          </div>
-          <div className="list-item__dkp--gained">
-            Lifetime Gained: {player.lifetimegained}
-          </div>
-          <div className="list-item__dkp--spent">
-            Lifetime Spent: {Number(player.lifetimespent) * -1}
-          </div>
-        </div>
-      </div>
-    ))}
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">Player</th>
+          <th scope="col">Current DKP</th>
+          <th scope="col">Lifetime Gained DKP</th>
+          <th scope="col">Lifetime Spent DKP</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.data.map((player, index) => (
+          <tr>
+            <th scope="row">
+              <div>{player.player}</div>
+              {player.class}
+              <div>
+                <img
+                  src={`https://wow.zamimg.com/images/wow/icons/large/classicon_${player.class.toLowerCase()}.jpg`}
+                  alt=""
+                />
+              </div>
+            </th>
+            <td colspan="2">{player.dkp}</td>
+            <td>{player.lifetimegained}</td>
+            <td>{player.lifetimespent}</td>
+          </tr>
+        ))}
+
+        <tr></tr>
+      </tbody>
+    </table>
   </div>
 );
 
